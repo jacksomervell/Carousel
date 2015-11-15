d203294c0466932c60d3ca1ffbebfd8f0eeb9fbc
Jack Somervell <jacksomervell@gmail.com>
2015-11-15 13:32:33 +0000

    set up basic html and access token

---
+++ Carousel.js
@@ -0,0 +1,13 @@
+     <script>
+     $0pop(document).ready(function(){
+          $0pop('#myCarousel').capiCarousel({
+               campaignId : 9884,
+               access_token : 'R3ggcIaeRA665XN8J4Qlbeg5LTvUAN',
+               media_type: 'image',
+               items: 5,
+               order: 'random_sort',
+               slideSpeed: 4000,
+               social_platform: 'twitter,instagram'
+          });
+     });
+     </script>
